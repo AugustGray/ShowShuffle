fd392f9aa3eb5eb7786464cf6fed9e2c94b11652
August Gray <63071966+AugustGray@users.noreply.github.com>
2025-11-29 19:52:20 -0600

    updated script.js and style.css to make button use user theme at first load + make button on top right corner on smartphone devices

--- script.js
+++ script.js
@@ -248,6 +248,14 @@ const htmlEl = document.documentElement;
 const storedTheme = localStorage.getItem('theme');
 if (storedTheme) {
 htmlEl.setAttribute('data-theme', storedTheme);
+} else {
+  // Default to dark theme if no preference is set in localStorage
+  const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  if (userPrefersDark) {
+    htmlEl.setAttribute('data-theme', 'dark');
+  } else {
+    htmlEl.setAttribute('data-theme', 'light');
+  }
 }
 themeToggleBtn.addEventListener('click', () => {
 const currentTheme = htmlEl.getAttribute('data-theme');
